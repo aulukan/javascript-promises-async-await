3ab94e2b7d41ae8c648efc9b057a01d8e42ef348
nara <aulukan@gmail.com>
2023-04-16 10:19:54 -0400

    completed module 7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {fetchBooks, fetchMovies} from './services';
+import {fetchBooks, fetchMovies, asyncFetchBooks, asyncFetchMovies} from './services';
 
 const movies = require('./data/movies.json');
 
@@ -24,3 +24,46 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
     console.log('getBooksOrMoviesPromise', results);
 })
+
+
+async function getBooksAndMoviesAsync(){
+    try{
+        const [books, movies] = await Promise.all([
+            asyncFetchBooks(),
+            asyncFetchMovies()
+            ]);
+            return {
+                books,
+                movies,    
+            }
+    }catch(error){
+        console.error("Error fetching books and movies", error);
+    }
+}
+
+async function getBooksOrMoviesAsync(){
+    try{
+      const values = await Promise.race([
+        asyncFetchBooks(),
+        asyncFetchMovies()
+        ]);
+        return values;
+    }catch(error){
+        console.error("Error waiting for the promise race", error)
+    }
+}
+
+getBooksAndMoviesAsync()
+.then(results => {
+    console.log("movies AND books", {
+        movies: results.movies,
+        books: results.books,
+      });
+});
+
+getBooksOrMoviesAsync()
+.then(results =>{
+    console.log("movies OR books", {
+        results,
+      });
+});
