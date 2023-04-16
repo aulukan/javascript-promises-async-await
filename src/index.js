961757e1fad41d2ebb4e6b1fcbf759457ba71576
nara <aulukan@gmail.com>
2023-04-16 08:59:30 -0400

    module competed 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import {fetchWithTimeout} from './services'
-const movies = require('./data/movies.json')
-export function fetchMovies() {
-    const  resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
-}
+import {fetchBooks, fetchMovies} from './services';
 
-const moviepromise = fetchMovies();
-moviepromise.then(results =>{
-    console.log(results)
+const movies = require('./data/movies.json');
+
+const  getBooksAndMovies = () =>{
+    return Promise.all([fetchMovies(), fetchBooks()])
+    .then(([books, movies])=>({books, movies,}))
+    .catch(error => console.log("Error fetching books and movies", error));
+};
+
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksAndMoviesPromise', results)
+})
+
+
+const  getBooksOrMovies = () =>{
+    return Promise.race([fetchMovies(), fetchBooks()])
+    .then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+};
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => {
+    console.log('getBooksOrMoviesPromise', results);
 })
